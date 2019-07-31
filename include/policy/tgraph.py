aa5969c29431d524238a7bae69d007761db17161
f4tsang <f4tsang@uwaterloo.ca>
2019-07-31 02:18:08 -0400

    updated tgraph module and tests, also updated tristan_maze graph and polygons

--- include/policy/tgraph.py
+++ include/policy/tgraph.py
@@ -12,7 +12,8 @@ logger = logging.getLogger(__name__)
 
 import networkx as nx
 from matplotlib import pyplot as plt
-from shapely.geometry import Point, LineString, Polygon
+from shapely.geometry import Point, LineString, box
+import csv
 
 # import timing
 
@@ -29,7 +30,7 @@ class TGraph(object):
         - an edge is unblocked if the robot can travel to the other end
         - edge is blocked otherwise
 
-    Polygons, points, lines are defined using Shapely library
+    boxes, points, lines are defined using Shapely library
     """
 
     def __init__(self, nxgraph, polygon_dict):
@@ -38,24 +39,71 @@ class TGraph(object):
                 - vertex attr: defn: <point, line, or multipoints>
         polygon_dict - dict: {edge: polygon, ...}
         """
+        self.graph = nxgraph
+
         logger.info("Validating input...")
         # check to make sure each edge has an associated polygon
-        for edge in nxgraph.edges():
+        for u,v in self.graph.edges():
             # set edge attributes
-            nxgraph.edge[edge]['state'] = UNKNOWN
-            nxgraph.edge[edge]['weight'] = self._calc_init_weight(edge)
-            # nxgraph.edge[edge]['polygon'] = polygon_dict[edge]
+            self.set_edge_state(u, v, UNKNOWN)
+            self.set_edge_weight(u, v, self.min_dist(u,v))
+            try:
+                self.graph.edge[u][v]['polygon'] = polygon_dict[(u,v)]
+            except KeyError:
+                self.graph.edge[u][v]['polygon'] = polygon_dict[(v,u)]
 
-        self.graph = nxgraph
-        self.polygon_dict = polygon_dict
-        # print(self.graph.edges())
+            # TODO: check that straight line between portals ('edge') is contained in polygon
 
-    def _calc_init_weight(self, edge):
-        # estimate initial weight of edges by calc min distance from one vertex to another
-        (v1, v2) = edge
-        a = self.graph.node[v1]['defn']
-        b = self.graph.node[v2]['defn']
+        logger.debug(self.graph.edges(data=True))
+
+    def min_dist(self, u, v):
+        # calculate shortest distance between two vertices
+        # If u and v are lines, then the shortest dist b/w the two lines are returned
+        a = self.graph.node[u]['defn']
+        b = self.graph.node[v]['defn']
         return a.distance(b)
+
+    def get_polygon(self, u, v):
+        # returns polygon associated w/ edge
+        return self.graph.edge[u][v]['polygon']
+
+    def get_vertices_in_polygon(self, des_poly):
+        # polygon - shapely polygon
+        # Returns all vertices that are a portal or are in des_poly
+        # Possible speed up here if needed
+        vset= set()
+
+        for u,v,poly in self.graph.edges(data='polygon'):
+            if poly == des_poly: # should not cause problems, otherwise use poly.equals(des_poly)
+                vset.update([u,v])
+
+        return vset
+    
+    def set_edge_state(self, u, v, state):
+        assert (state == UNBLOCKED or state == BLOCKED or state == UNKNOWN), (
+            "Attempted to assign invalid state ({}) to edge ({},{}).".format(state, u, v))
+
+        self.graph.edge[u][v]['state'] = state
+
+    def set_edge_weight(self, u, v, weight):
+        assert isinstance(weight, (float, int, long)), (
+            "Attempted to assign a non-number ({}) to edge ({},{}).".format(weight, u, v))
+
+        self.graph.edge[u][v]['weight'] = weight
+
+    def check_edge_state(self, u, v, rospath):
+        # (u,v) - edge we want to check
+        # rospath - arr of PoseStamped
+        # Sets edge state: if rospath is contained in edge polygon -> edge is unblocked
+        # Also returns updated state of edge
+        raw_path = [stamped_pose.pose.position for stamped_pose in rospath]
+        path = LineString(raw_path)
+
+        polygon = self.get_polygon(u,v)
+        if polygon.contains(path): self.set_edge_state(u,v,UNBLOCKED)
+        else: self.set_edge_state(u,v,BLOCKED)
+
+        return self.graph.edge[u][v]['state']
 
     def draw_vertices(self, ax):
         # vertices are points or portals/lines
@@ -72,9 +120,33 @@ class TGraph(object):
     def draw_polygons(self, ax, edges=[]):
         # draw polygons
         # either draw all of them by default, or just the ones specified by edges
-        if is_empty(edges): edges = self.graph.edges()
+        if edges == []: edges = self.graph.edges()
 
-        for edge in edges:
-            poly = self.graph.edge[edge]['polygon']
+        for u,v in edges:
+            poly = self.graph.edge[u][v]['polygon']
             X,Y = poly.exterior.xy
             ax.plot(X,Y,'r')
+
+def polygon_dict_from_csv(path):
+    # each line in csv should be in following format:
+    #   u,v,minx,maxx,miny,maxy
+    # Assumes all polygons are boxes
+
+    poly_dict = {}
+
+    with open(path, 'rb') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        lines = []
+        for row in data:
+            logger.debug(row)
+            u = row[0]
+            v = row[1]
+            if u.isdigit(): u = int(u)
+            if v.isdigit(): v = int(v)
+            minx, maxx, miny, maxy = row[2:]
+
+            # convert to box
+            poly = box(float(minx), float(miny), float(maxx), float(maxy))
+            poly_dict[(u,v)] = poly
+
+    return poly_dict
--- tests/testTGraph.py
+++ tests/testTGraph.py
@@ -9,28 +9,56 @@ import cv2
 import policy.tgraph as tgraph
 from policy import utility as util
 
+TESTPATH = os.path.dirname(os.path.abspath(__file__))
+
 def get_bounds_from_yaml(grid, yamlfile):
     origin, res = util.get_origin_and_res_from_yaml(yamlfile)
     iheight, iwidth = grid.shape
     return util.calc_bounding_coord_of_grid(origin, iwidth, iheight, res)
 
-
-if __name__ == '__main__':
-    testpath = os.path.dirname(os.path.abspath(__file__))
-    filename = '00'
-    gridfile = testpath + '/results/tristan maze/{}.pgm'.format(filename)
-    yamlfile = testpath + '/results/tristan maze/{}.yaml'.format(filename) 
-    
-    logging.basicConfig(level=logging.DEBUG)
+def test_drawing(G):
+    # G - tgraph type
+    filename = '/results/tristan maze/00'
+    gridfile = TESTPATH + '{}.pgm'.format(filename)
+    yamlfile = TESTPATH + '{}.yaml'.format(filename) 
 
     img = cv2.imread(gridfile, cv2.IMREAD_GRAYSCALE)
     bounds = get_bounds_from_yaml(img, yamlfile)
 
-    graph = nx.read_yaml(testpath + '/tristan_maze_tgraph.yaml')
-    tgraph1 = tgraph.TGraph(graph, {})
-
     fig, ax1 = plt.subplots(1,1)
     ax1.imshow(img, cmap='gray', interpolation='bicubic', extent=bounds)
-    tgraph1.draw_vertices(ax1)
+    G.draw_vertices(ax1)
+    G.draw_polygons(ax1)
     plt.show()
 
+def test_get(G):
+    print("\nTesting get_polygon and get_vertices_in_polygon...")
+
+    for u,v in G.graph.edges():
+        polygon = G.get_polygon(u,v)
+        logger.debug(list(polygon.exterior.coords))
+        vset = G.get_vertices_in_polygon(polygon)
+        logger.debug(list(vset))
+
+        # validation
+        while vset != set():
+            a = vset.pop()
+            for b in vset:
+                logger.debug("a: {}, b: {}".format(a,b))
+                if G.get_polygon(a,b) != polygon:
+                    raise Exception("Returned polygon for ({},{}) doesn't match ({},{})"
+                                    .format(a,b,u,v))
+
+    print("Test passed\n")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    # init TGraph class
+    graph = nx.read_yaml(TESTPATH + '/tristan_maze_tgraph.yaml')
+    poly_dict = tgraph.polygon_dict_from_csv(TESTPATH + '/tristan_maze_polygons.csv')
+    tgraph1 = tgraph.TGraph(graph, poly_dict)
+
+    test_get(tgraph1)
+    test_drawing(tgraph1)
