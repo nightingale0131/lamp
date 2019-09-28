8fcfc252b63bde292da717d49c7ad1b89d73bfac
f4tsang <f4tsang@uwaterloo.ca>
2019-09-28 01:36:46 -0400

    corrected map filter to only merge if newmap is not a subset of any other supermap

--- include/policy/mapfilters.py
+++ include/policy/mapfilters.py
@@ -13,8 +13,9 @@ import networkx as nx
 
 def filter1(supermaps, new_map):
     """
-    This filter only merges maps with the first encountered agreeing map,
-    but it will update n of all agreeing maps
+    This filter only merges maps with the first encountered agreeing map, provided it is
+    not a subset of any other map.
+    It will update n of all agreeing maps, regardless of whether it is merging or not.
 
     supermaps - list of type Maps
     new_map - type Map
@@ -26,15 +27,17 @@ def filter1(supermaps, new_map):
     for i,m in enumerate(supermaps):
         in_agreement, new_info = m.agrees_with(new_map)
         if in_agreement:
-            agree_once = True
             m.n += 1
             logger.info("New map agrees with supermap {}".format(i))
 
-            if (new_info != {} and map_merge == None):
-                logger.info(new_info)
+            if (new_info != {} and agree_once == False):
                 map_merge = m
                 n_map_merge = i
                 update = new_info
+            elif new_info == {}:
+                map_merge = None
+
+            agree_once = True
 
     if not agree_once:
         # no maps were in agreement, add new map to supermaps
@@ -43,6 +46,7 @@ def filter1(supermaps, new_map):
     elif map_merge != None:
         # merge
         print("Merging to map {}".format(n_map_merge))
+        logger.info(new_info)
         map_merge.updateG(update)
     else:
         print("No map added")
--- tests/testMapAgreement.py
+++ tests/testMapAgreement.py
@@ -31,6 +31,7 @@ def setup_graph1(G):
     logger.info("Successfully setup graph1")
 
 def setup_graph2(G):
+    # map to be merged with graph1
     G.set_edge_state('s', 7, G.UNBLOCKED)
     G.set_edge_state(6, 7, G.UNBLOCKED)
     G.set_edge_state(6, 5, G.UNBLOCKED)
@@ -48,6 +49,46 @@ def setup_graph2(G):
 
     logger.info("Successfully setup graph2")
 
+def setup_graph3(G):
+    # disagree with graph2
+    G.set_edge_state('s', 7, G.UNBLOCKED)
+    G.set_edge_state(6, 7, G.UNBLOCKED)
+    G.set_edge_state(6, 8, G.UNBLOCKED)
+    G.set_edge_state(6, 5, G.UNBLOCKED)
+    G.set_edge_state(5, 12, G.UNBLOCKED)
+    G.set_edge_state(9, 16, G.UNBLOCKED)
+    G.set_edge_state(9, 'g', G.UNBLOCKED)
+
+    set_blocked(G, 13, 16)
+    set_blocked(G, 13, 12)
+    set_blocked(G, 4, 5)
+    set_blocked(G, 4, 12)
+    set_blocked(G, 11, 13)
+    set_blocked(G, 8, 16)
+
+    logger.info("Successfully setup graph3")
+
+def setup_graph4(G):
+    # agree with graph2 w/o merging
+    G.set_edge_state('s', 7, G.UNBLOCKED)
+    G.set_edge_state(6, 7, G.UNBLOCKED)
+    G.set_edge_state(6, 5, G.UNBLOCKED)
+    G.set_edge_state(5, 12, G.UNBLOCKED)
+    G.set_edge_state(13, 12, G.UNBLOCKED)
+    G.set_edge_state(13, 16, G.UNBLOCKED)
+    G.set_edge_state(9, 16, G.UNBLOCKED)
+    G.set_edge_state(9, 10, G.UNBLOCKED)
+    G.set_edge_state(9, 'g', G.UNBLOCKED)
+
+    set_blocked(G, 6, 8)
+    set_blocked(G, 7, 8)
+    set_blocked(G, 4, 5)
+    set_blocked(G, 4, 12)
+    set_blocked(G, 11, 13)
+    set_blocked(G, 8, 16)
+
+    logger.info("Successfully setup graph4")
+
 def set_blocked(G, u, v):
     G.set_edge_state(u, v, G.BLOCKED)
     G.set_edge_weight(u, v, float('inf'))
@@ -55,22 +96,34 @@ def set_blocked(G, u, v):
 if __name__ == '__main__':
     testpath = os.path.dirname(os.path.abspath(__file__))
     # logging.basicConfig(filename=testpath + '/testMapAgreement_debug.log', filemode='w',level=logging.INFO)
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     graph = nx.read_yaml('../maps/tristan_maze/tristan_maze_tgraph.yaml')
     poly_dict = tgraph.polygon_dict_from_csv('../maps/tristan_maze/tristan_maze_polygons.csv')
 
     test_tgraph1 = tgraph.TGraph(graph, poly_dict)
     test_tgraph2 = tgraph.TGraph(graph, poly_dict)
+    test_tgraph3 = tgraph.TGraph(graph, poly_dict)
+    test_tgraph4 = tgraph.TGraph(graph, poly_dict)
 
     setup_graph1(test_tgraph1)
     setup_graph2(test_tgraph2)
+    setup_graph3(test_tgraph3)
+    setup_graph4(test_tgraph4)
 
     map1 = Map(copy(test_tgraph1))
+    map1_copy = Map(copy(test_tgraph1))
     map2 = Map(copy(test_tgraph2))
+    map3 = Map(copy(test_tgraph3))
+    map4 = Map(copy(test_tgraph4))
 
-    mf.filter1([map1], map2)
+    logger.info("Following should be a successful merge with map 0")
+    mf.filter1([map1, map3, map1_copy], map2)
 
-    logger.info(map1.G)
+    logger.info("No merge should happen.")
+    mf.filter1([map1_copy, map3, map4], map2)
+
+    logger.info("Map should be added.")
+    mf.filter1([map3], map2)
 
     logger.info("Test passed")
