f097b23fc7b8f41783c4aa4350e68857ec4fed05
f4tsang <f4tsang@uwaterloo.ca>
2019-10-03 00:54:45 -0400

    added condition for edge weight to only be updated if it is known in the new_map
    Former-commit-id: 91b2de507118f75e3976cb27fcc19cec72415bbb

--- include/policy/mapfilters.py
+++ include/policy/mapfilters.py
@@ -30,10 +30,12 @@ def update_weights(supermaps, new_map):
     new_G = copy(new_map.G) # in case new_map was added to supermaps
     for m in supermaps:
         for (u,v) in m.G.edges():
-            if new_G.weight(u,v) != float('inf'):
+            if (new_G.weight(u,v) != float('inf') and 
+                new_G.edge_state(u,v) == new_G.UNBLOCKED):
                 old_weight = m.G.weight(u,v)
                 new_weight = util.moving_average(old_weight, new_G.weight(u,v))
                 m.G.set_edge_weight(u,v,new_weight)
+        m.update_cost(m.G.goal)
 
     return supermaps
 
