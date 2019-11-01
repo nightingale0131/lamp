5de7ef9d190b9341a808a2c2337f06fa6324fc9e
f4tsang <f4tsang@uwaterloo.ca>
2019-11-01 19:48:22 -0400

    updated lamp_rescollect to handle result files with all 3 costfns
    Former-commit-id: bcef73c9b7a3aca3ce8a22579ca7b05ad92cfe6e

--- include/policy/lamp_rescollect.py
+++ include/policy/lamp_rescollect.py
@@ -2,18 +2,42 @@
 # Author: Florence Tsang
 # Creation date: 2019-10-04
 
-import os
+import os, sys, getopt
 import utility as util
 
+def parse_command_line(argv):
+    res_file = "lrpp_results.dat"
+    n = None
+
+    try:
+        opts, args = getopt.getopt(argv, "hf:n:")
+    except getopt.GetoptError:
+        print("lamp_rescollect.py [-f <file> -n <tasks to process>]")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print("lamp_rescollect.py [-f <file> -n <tasks to process>]")
+            sys.exit()
+
+        elif opt == "-f": res_file = arg.strip()
+        elif opt == "-n": n = int(arg)
+
+    return res_file, n
+
 if __name__ == '__main__':
-    filepath = raw_input('Which file to analyze? Relative from current directory:\n')
+    # filepath = raw_input('Which file to analyze? Relative from current directory:\n')
+    filepath, n = parse_command_line(sys.argv[1:])
 
     filename, ext = os.path.splitext(filepath)
     if ext != '.dat':
         print('Invalid file!')
         sys.exit()
 
-    policy_data = []
+    # policy_data = []
+    policy1_data = []
+    policy2_data = []
+    policy3_data = []
     openloop_data = []
     naive_data = []
 
@@ -22,9 +46,20 @@ if __name__ == '__main__':
         print('Analyzing {}...'.format(filepath))
 
         for line in datafile:
+            '''
             if line.startswith("Policy"):
                 parts = line.split()
                 policy_data.append(float(parts[1]))
+            '''
+            if line.startswith("Policy 1"):
+                parts = line.split()
+                policy1_data.append(float(parts[2]))
+            if line.startswith("Policy 2"):
+                parts = line.split()
+                policy2_data.append(float(parts[2]))
+            if line.startswith("Policy 3"):
+                parts = line.split()
+                policy3_data.append(float(parts[2]))
             if line.startswith("Openloop"):
                 parts = line.split()
                 openloop_data.append(float(parts[1]))
@@ -32,6 +67,12 @@ if __name__ == '__main__':
                 parts = line.split()
                 naive_data.append(float(parts[1]))
 
-    print("{:19}: {:.2f}".format("Policy average", util.average(policy_data)))
+            if len(naive_data) == n: break
+
+    print("Average over {} tasks:".format(len(naive_data)))
+    # print("{:19}: {:.2f}".format("Policy average", util.average(policy_data)))
+    print("{:19}: {:.2f}".format("Policy 1 average", util.average(policy1_data)))
+    print("{:19}: {:.2f}".format("Policy 2 average", util.average(policy2_data)))
+    print("{:19}: {:.2f}".format("Policy 3 average", util.average(policy3_data)))
     print("{:19}: {:.2f}".format("Openloop average", util.average(openloop_data)))
     print("{:19}: {:.2f}".format("Naive average", util.average(naive_data)))
