14d86d67160c86d055ebafa64660fa40277414fe
f4tsang <f4tsang@uwaterloo.ca>
2019-12-18 19:22:10 -0500

    updated make files to match updated repo structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-        packages=['policy'],
-        package_dir={'': 'include'},
+        packages=['lamp'],
+        package_dir={'': 'src'},
 )
 setup(**setup_args)
